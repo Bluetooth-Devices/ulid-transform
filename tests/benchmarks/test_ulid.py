a0bc12709edd9030cff0afc4be91f1994674a7b7
J. Nick Koston <nick@home-assistant.io>
2026-03-14 13:36:50 -1000

    Revert "fix: run benchmarks in tight loop for CodSpeed accuracy"
    This reverts commit ab77336e5e3e5d2bce1b313f3187d3a05874824f.

--- tests/benchmarks/test_ulid.py
+++ tests/benchmarks/test_ulid.py
@@ -2,31 +2,18 @@ from pytest_codspeed import BenchmarkFixture
 
 from ulid_transform import ulid_at_time, ulid_at_time_bytes, ulid_now
 
-ITERATIONS = 10000
-
 
 def test_ulid_now(benchmark: BenchmarkFixture) -> None:
-    _ulid_now = ulid_now
-
-    @benchmark
-    def _():
-        for _ in range(ITERATIONS):
-            _ulid_now()
+    benchmark(ulid_now)
 
 
 def test_ulid_at_time(benchmark: BenchmarkFixture) -> None:
-    _ulid_at_time = ulid_at_time
-
     @benchmark
     def _():
-        for _ in range(ITERATIONS):
-            _ulid_at_time(1)
+        ulid_at_time(1)
 
 
 def test_ulid_at_time_bytes(benchmark: BenchmarkFixture) -> None:
-    _ulid_at_time_bytes = ulid_at_time_bytes
-
     @benchmark
     def _():
-        for _ in range(ITERATIONS):
-            _ulid_at_time_bytes(1)
+        ulid_at_time_bytes(1)
