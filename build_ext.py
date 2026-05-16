e1e0d8a0f5b12e7055f37569aecfa8e5179c3438
J. Nick Koston <nick@home-assistant.io>
2026-05-16 16:41:50 -0700

    fix(build): silence mypy on inherited parallel attribute

--- build_ext.py
+++ build_ext.py
@@ -34,7 +34,7 @@ ulid_module = Extension(
 
 class BuildExt(build_ext):
     def build_extensions(self) -> None:
-        if self.parallel is None:
+        if self.parallel is None:  # type: ignore[has-type, unused-ignore]
             self.parallel = os.cpu_count() or 1
         try:
             super().build_extensions()
